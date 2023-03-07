7781da0adfa3e289297ddf2defb024a4cf239219
Serge Sharoff <serge.sharoff@gmail.com>
2023-03-07 17:50:34 +0000

    adding russian-turkish cognates

--- align-spaces.sh
+++ align-spaces.sh
@@ -14,12 +14,13 @@ OUTPUT='out'
 L1=$1  # en
 L2=$2  # it
 SUF=$3 # 300-fasttext.dat
+# suffix from a better dicy
 
 SRC_EMB=$L1-$SUF
 TRG_EMB=$L2-$SUF
-SRCTRG_EMB=$L2$L1-$SUF
+SRCTRG_EMB=$L2$L1$4-$SUF
 
-TRAIN_DIC=$L1-$L2-train.dic
+TRAIN_DIC=$L1-$L2-train$4.dic
 TEST_DIC=$L1-$L2-test.dic
 
 mkdir -p $OUTPUT/
@@ -33,9 +34,10 @@ if [ ! -f $OUTPUT/$TRG_EMB ]; then
   time python3 src/normalize_embeddings.py unit center -i $DATA/$TRG_EMB -o $OUTPUT/$TRG_EMB
 fi
 
-echo "building mapping for $SRC_EMB using $TRAIN_DIC"
 if [ ! -f $OUTPUT/$SRCTRG_EMB ]; then
+  echo "building mapping for $SRC_EMB using $TRAIN_DIC"
   time python3 src/project_embeddings.py --orthogonal $OUTPUT/$SRC_EMB $OUTPUT/$TRG_EMB -d $DATA/$TRAIN_DIC -o $OUTPUT/$SRCTRG_EMB
 fi
 
-python3 src/eval_translation1.py -1 $OUTPUT/$SRCTRG_EMB -2 $OUTPUT/$TRG_EMB -d $DATA/$TEST_DIC >$L1-$L2.out
+echo "Evaluating $SRCTRG_EMB into $TRG_EMB on $TEST_DIC"
+python3 src/eval_translation1.py -1 $OUTPUT/$SRCTRG_EMB -2 $OUTPUT/$TRG_EMB -d $DATA/$TEST_DIC -l $DATA/$L1-$L2.cost >$L1-$L2.out
